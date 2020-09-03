22aecc388381384a254306611122e5a8ddb7fb16
JeranDev <jeranjb@gmail.com>
2020-09-03 17:41:20 -0600

    added a handler for the form

---
+++ contact-form-handler.php
@@ -0,0 +1,18 @@
+<?php
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email_subject = 'New Form Submission';
+    $visitor_email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+    
+    $email_body = "Name: $name.\n" . "Email: $visitor_email.\n" . "Phone Number: $phone.\n". "Message: $message.\n";
+
+    $mailTo = 'jerandev@outlook.com';
+    $headers = "From: $visitor_email \r\n";
+    $txt = "You have received an e-mail from $name. \n\n $email_body";
+
+    mail($mailTo, $email_subject, $txt, $headers);
+    header("Location: index.html");
+}
+?>
